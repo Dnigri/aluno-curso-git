42373edab6d5d3af675eda8c232b17b88857b32e
Daniel Nigri <nigri05@hotmail.com>
2020-03-28 23:29:50 -0300

    Acrescentado BOM DIA

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace teste_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("bom dia");
         }
     }
 }
